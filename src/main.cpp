c92470f403cc59be93ea9753c5676918ca06ea90
sijah.ak <sijah.ak@gmail.com>
2020-08-04 14:05:42 +0530

    establishlfs

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,38 @@
 
 const char *ssid = "CABIN1";
 const char *pass = "asdfghjkl";
+ESP8266WebServer server(80);
+
+void handle_root()
+{
+File f = LittleFS.open("/index.html","r");
+  server.streamFile(f, "text/html");
+  //file.close();
+  //server.send(200,"text/html",f);
+
+
+}
+
+void handle_notfound()
+{
+
+  server.send(404, "text/html", "jango pettu");
+}
 
 void setup()
+
 {
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
+    Serial.begin(115200);
+delay(100);
+ if(LittleFS.begin())
+ {
+     Serial.print("littlefson");
+ }
+ else
+ {
+     Serial.print("littlefs no working");
+ }
+   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
   {
     Serial.print(".");
@@ -20,10 +47,72 @@ void setup()
   Serial.print(ssid);
   Serial.print("connected ip");
   Serial.println(WiFi.localIP());
+   if (!MDNS.begin("esp8266"))
+  {
+    Serial.println("Error setting up MDNS responder!");
+    while (1)
+    {
+      delay(1000);
+    }
+  }
   // put your setup code here, to run once:
+FSInfo fs_info;
+    LittleFS.info(fs_info);
+ 
+    Serial.println("File sistem info.");
+ 
+    Serial.print("Total space:      ");
+    Serial.print(fs_info.totalBytes);
+    Serial.println("byte");
+ 
+    Serial.print("Total space used: ");
+    Serial.print(fs_info.usedBytes);
+    Serial.println("byte");
+ 
+    Serial.print("Block size:       ");
+    Serial.print(fs_info.blockSize);
+    Serial.println("byte");
+ 
+    Serial.print("Page size:        ");
+    Serial.print(fs_info.totalBytes);
+    Serial.println("byte");
+ 
+    Serial.print("Max open files:   ");
+    Serial.println(fs_info.maxOpenFiles);
+ 
+    Serial.print("Max path lenght:  ");
+    Serial.println(fs_info.maxPathLength);
+ 
+    Serial.println();
+ 
+    // Open dir folder
+    Dir dir = LittleFS.openDir("/");
+    // Cycle all the content
+    while (dir.next()) {
+        // get filename
+        Serial.print(dir.fileName());
+        Serial.print(" - ");
+        // If element have a size display It else write 0
+        if(dir.fileSize()) {
+            File f = dir.openFile("r");
+            Serial.println(f.size());
+            f.close();
+        }else{
+            Serial.println("0");
+        }
+    }
+server.on("/",handle_root);
+server.onNotFound(handle_notfound);
+
+server.begin();
+
+
+
 }
+
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+   MDNS.update();
+  server.handleClient();
 }
